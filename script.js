35562ff18e7da63f3999e20303f973dc8d375bba
designcluster7-cmd <designcluster7@gmail.com>
2026-09-23 07:33:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,21 @@ document.querySelectorAll(".sample-btn").forEach(button => {
     button.textContent = "⏸ Playing...";
   });
 });
+
+// =========================
+// SKYWOLF MOBILE MENU
+// =========================
+
+const menuButton = document.querySelector(".menu");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+
+    const isOpen = navigation.classList.contains("open");
+
+    menuButton.setAttribute("aria-expanded", isOpen);
+
+});
+
